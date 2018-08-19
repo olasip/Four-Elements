2df68dcf47a0d9e9226d62548daf15c7f9aa1624
Aleksandra <aleksandrysip@gmail.com>
2018-08-19 17:02:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aleksandra/fourelements/PageOne.kt
@@ -0,0 +1,12 @@
+package com.example.aleksandra.fourelements
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PageOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_one)
+    }
+}
