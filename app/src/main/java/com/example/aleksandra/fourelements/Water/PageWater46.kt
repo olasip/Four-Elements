e1406e44c0f52e755dcec7e9206241c4da0b5009
Aleksandra <aleksandrysip@gmail.com>
2018-08-21 16:37:09 +0200

    fix hardcoded texts

--- app/src/main/java/com/example/aleksandra/fourelements/Water/PageWater46.kt
+++ app/src/main/java/com/example/aleksandra/fourelements/Water/PageWater46.kt
@@ -1,4 +1,4 @@
-package com.example.aleksandra.fourelements
+package com.example.aleksandra.fourelements.Water
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,5 +9,8 @@ class PageWater46 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_water46)
+
+
+
     }
 }
--- app/src/main/java/com/example/aleksandra/fourelements/Water/PageWater47.kt
+++ app/src/main/java/com/example/aleksandra/fourelements/Water/PageWater47.kt
@@ -2,6 +2,7 @@ package com.example.aleksandra.fourelements.Water
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aleksandra.fourelements.R
 
 class PageWater47 : AppCompatActivity() {
 
