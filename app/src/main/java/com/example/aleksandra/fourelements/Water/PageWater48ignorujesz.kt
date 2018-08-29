b71cb949c78165470cb1f039d8041a9b537c8f38
Aleksandra <aleksandrysip@gmail.com>
2018-08-29 09:02:00 +0200

    fix hardcoded texts

---
+++ app/src/main/java/com/example/aleksandra/fourelements/Water/PageWater48ignorujesz.kt
@@ -0,0 +1,13 @@
+package com.example.aleksandra.fourelements.Water
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aleksandra.fourelements.R
+
+class PageWater48ignorujesz : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_water48ignorujesz)
+    }
+}
